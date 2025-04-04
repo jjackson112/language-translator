2ca1986dda50514df55d53e206c8a8afd7eeb09f
jjackson112 <jjackson11211@gmail.com>
2025-04-04 09:33:18 -0400

    User friendly - intro and exit functions

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import csv
+# create an intro function to direct the user and state purpose of app
+def intro()
+  print(f'Welome to the Spanish and French translator app. \nPlease enter an English word and press the "Enter" key.')
+  print(f' \nType "done" at any time to exit.')
+
+# create an exit function
+def exit()
+  print(f'\nThanks for using the translator app. Have a good day!')
 
 translations = {}
 
 with open("translations.csv", "r") as words:
-reader = csv.DictReader(words, delimiter=",")
+  reader = csv.DictReader(words, delimiter=",")
 for line in reader:
   english = line["English"]
   line = ["English"].lower()
@@ -17,7 +25,8 @@ for line in reader:
 translations[english] = [spanish, french]
 
 done = False
-print('Type "done" at any time to exit')
+# call intro function
+intro()
 
 # setting the while loop to not done means the loop will run as long as done is false
 while not done:
@@ -26,8 +35,13 @@ while not done:
   if word == "done":
     # setting done to true will end the loop
     done == True
+    exit()
+    # adjust the output of Spanish and French translations
+    # word variable is the dictionary key; [0] is the Spanish word in index 0 - they were set here when spanish was placed in the first position of the dictionary keys
   elif word in translations:
-    print(translations[word])
+    print(f'\nSPANISH:{translations[word][0]}')
+  elif word in translations:
+    print(f'\nFRENCH:{translations[word][1]}')
   else:
     print("Sorry that word is not listed in the dictionary.")
 
